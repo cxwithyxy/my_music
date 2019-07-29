af5ac527695c070862ac4380a553ccb34ddec908
chenxuan <523764720@qq.com>
2019-07-29 15:01:12 +0800

    为了方便数据的操作, 把josn控制器改成了多线程, 而不是多进程了

--- file_io/Json_controller.py
+++ file_io/Json_controller.py
@@ -1,14 +1,13 @@
 import json
-from multiprocessing import Process
-from multiprocessing.managers import BaseManager
+from threading import Thread
 import time
 import pathlib
 
 class Json_controller:
     
     path:str
-    data = None
-    auto_save_thread:Process = None
+    json_data = None
+    auto_save_thread:Thread = None
 
     def file_is_exists(self):
         return pathlib.Path(self.path).exists()
@@ -17,29 +16,30 @@ class Json_controller:
         self.path = path
 
     def get_data(self):
-        if(self.data):
-            return self.data
-        self.data = json.load(open(self.path, encoding="utf8"))
-        return self.data
+        if(self.json_data):
+            return self.json_data
+        self.json_data = json.load(open(self.path, encoding="utf8"))
+        return self.json_data
     
     def set_data(self, content):
-        self.data = content
+        self.json_data = content
 
     def save(self):
-        json.dump(self.data, open(self.path, "w", encoding="utf8"))
+        json.dump(self.json_data, open(self.path, "w", encoding="utf8"))
 
     def thread_do(self, loop_time: int):
         while True:
             time.sleep(loop_time)
             self.save()
+            print(f"save: {(self.json_data)}")
 
     def auto_save_start(self, loop_time: int):
         if(self.auto_save_thread):
             return
-        self.auto_save_thread = Process(
-            name = f"json_auto_save: {self.path}",
+        self.auto_save_thread = Thread(
             target = self.thread_do,
-            args=(loop_time,)
+            args=(loop_time,),
+            daemon=True
         )
         self.auto_save_thread.start()
 
