32c7ed09c9945a7e1f1404cc9c285a5399464012
chenxuan <523764720@qq.com>
2019-07-31 10:35:46 +0800

    现在能把js代码自动复制到粘贴板了

--- controller/qq_music_list_httpserver.py
+++ controller/qq_music_list_httpserver.py
@@ -3,6 +3,8 @@ import threading
 import json
 from file_io.Config_controller import Config_controller as ConfC
 from file_io.Json_controller import Json_controller as JSONC
+import pyperclip
+from file_tools.file import get_file_string
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -41,8 +43,11 @@ class Main():
         self.confc.cd("path_set")
 
     def start(self):
+        self.code_to_clipboard()
         httpd = SimpleHTTPServer(('localhost', 8182), SimpleHTTPRequestHandler)
         print("\n\nqq_music_list receive server start \n\n")
+        print("打开 https://y.qq.com/portal/profile.html 并登陆")
+        print("按下 f12 打开调试台, 在代码区域 ctrl+v 粘贴代码, 按下回车运行")
         httpd.serve_forever()
         print(f"\nserver shutdown !")
         qq_music_list_path = self.confc.get("qq_music_list")
@@ -50,4 +55,8 @@ class Main():
         jsonc.set_data(httpd.song_list)
         jsonc.save()
         print(f"\nsave ({len(jsonc.get_data())}) in {qq_music_list_path} successfully !")
+    
+    def code_to_clipboard(self):
+        a = get_file_string("browser_script/get_qq_music_list.js", encoding="utf8")
+        pyperclip.copy(a)
 
